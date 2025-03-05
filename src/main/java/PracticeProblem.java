1d79e8b8c519f3c5837966c0beec1f7281ad1204
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-03-05 14:57:42 +0000

    finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,20 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+public static int goodString(String word){
+	String c;
+	int leastOp;
+	c = word.charAt(0) + "";
+	leastOp =  word.length() - (word.lastIndexOf(c) - word.indexOf(c) + 1) ;
+	for (int i = 1; i < word.length(); i++){
+c = word.charAt(i) + "";
+if(leastOp >  word.length() - (word.lastIndexOf(c) - word.indexOf(c) + 1)){
+		leastOp =  word.length() - (word.lastIndexOf(c) - word.indexOf(c) + 1) ;
+	}
+}
+if(word.length() - leastOp < 2){
+	return -1;
+}
+return leastOp;
+}
 }
